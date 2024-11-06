d2fe6a0394a2dd2727d5669527b98ffa2e956db6
Anna Prokopchenko <aprokopchenko@kse.org.ua>
2024-11-06 18:06:35 +0200

    Validations work now but it isn't final

--- Validations.py
+++ Validations.py
@@ -1,12 +1,4 @@
-def basic_validation(input) :
-    while True:
-        if input != None:
-            input = input.strip()
-            input = input.lower()
-            return input
-
-
-def right_base(base) :
+def right_base(base,number) :
     if base == 2 or base == 16 :
         number = number[2:]
     elif base < 9 :
@@ -23,22 +15,35 @@ def right_base(base) :
     return True
 
 
-def validate_number(number:str):
-    number = basic_validation(input=number)
-    if len(number) < 3 :
-        return False
-    #а десяткова система????????????????????
-    if number[0] == "0" or number[0] == "o" :
-        if number[1] == "b" :
-            old_base = 2
-        elif number[1] == "x" :
-            old_base = 16
-    elif number[len(number)-2] == "x" and number[len(number)-1].isnumeric() :
-        old_base = int(number[len(number)-1])
-    elif number[len(number)-3] == "x" and number[len(number)-2:].isnumeric() :
-        old_base = int(number[len(number)-2:])
-    if old_base != None :
-        right_base(base=old_base)
+def validate(number):
+    while True :
+        if number == None:
+            number = input("Sorry, try again\n:")
+            continue
+        if len(number)<3:
+            number = input("Sorry, try again\n:")
+            continue
+        number = number.strip()
+        number = number.lower()
+        if number[0] == "0" or number[0] == "o":
+            if number[1] == "b":
+                old_base = 2
+            elif number[1] == "x":
+                old_base = 16
+        elif number[len(number) - 2] == "x" and number[len(number) - 1].isnumeric():
+            old_base = int(number[len(number) - 1])
+        elif number[len(number) - 3] == "x" and number[len(number) - 2:].isnumeric():
+            old_base = int(number[len(number) - 2:])
+        else:
+            old_base = 10
+        if right_base(base=old_base, number=number) :
+            return number, old_base
+        else :
+            number = input("Sorry, try again\n:")
+
+
+
+
 
 
 
