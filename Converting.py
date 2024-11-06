0f091395027e3d86378c31b859a8038638551917
Anna Prokopchenko <aprokopchenko@kse.org.ua>
2024-11-06 18:24:31 +0200

    unary and decimal edge case in converting

--- Converting.py
+++ Converting.py
@@ -3,14 +3,18 @@ from Main_Body import ALL_NUMBERS
 def to_decimal(number:str,old_base):
     return sum([num * old_base ** ind for num,ind in enumerate(number[::-1])])
 
-def to_other(old_num_dec:int, new_base) :
+def to_other(old_num_dec:int, new_base:int) :
     new_number = ""
-    #unary makes an edge case
+    if new_base == 1 :
+        return "1"*old_num_dec
+    if new_base == 10 :
+        return old_num_dec
     while old_num_dec != 0 :
         new_number += ALL_NUMBERS[old_num_dec%new_base]
         old_num_dec //= new_base
     new_number = new_number[::-1]
     return new_number
+
 
 
 
