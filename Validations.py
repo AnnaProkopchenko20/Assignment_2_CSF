f02a8ad58e08aabd9609780493bc3eaaeda11af3
Anna Prokopchenko <aprokopchenko@kse.org.ua>
2024-11-07 12:42:19 +0200

    working on edge cases in validations

--- Validations.py
+++ Validations.py
@@ -3,7 +3,7 @@ def right_base(base,number) :
         number = number[2:]
     elif base < 9 :
         number = number[:len(number)-3]
-    else :
+    elif base != 10 :
         number = number[:len(number) - 4]
     allowed = [str(i) for i in range(base)]
     if base > 10 :
@@ -14,17 +14,21 @@ def right_base(base,number) :
             return False
     return True
 
-
+#could there be a problem with that magical ")" appearing in the end of each input?
 def validate(number):
     while True :
         if number == None:
             number = input("Sorry, try again\n:")
             continue
-        if len(number)<3:
+        if len(number)<3:#edge case for small numbers in decimal print(validate(0))
             number = input("Sorry, try again\n:")
             continue
         number = number.strip()
         number = number.lower()
+        if not number.isalnum() :
+            number = input("Sorry, try again\n:")# if you type in a lot of shifts it is a problem, i solved it but needs more testing
+            continue
+        #needs an extra function cause too messy
         if number[0] == "0" or number[0] == "o":
             if number[1] == "b":
                 old_base = 2
@@ -41,7 +45,7 @@ def validate(number):
         else :
             number = input("Sorry, try again\n:")
 
-
+#print(validate(input()))
 
 
 
